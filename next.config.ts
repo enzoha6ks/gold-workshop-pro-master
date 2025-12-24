c506df9027013be7ae7429f762db43498b7a57ce
enzoha6ks <qasimshahzad800@gmail.com>
2025-12-23 22:29:19 -0800

    updates

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+  },
 
 };
 
